3b9579efc25315e7c2561dc5f825b2fb8c2c407b
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-10-27 23:00:03 +0000

    Added back, forward, and new search functionality

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
 import requests
 import json
 
-query = input("What would you like to search? ")
-
-base_url = {
-    'book': f"https://www.googleapis.com/books/v1/volumes?q={query}",
-    'author': f"https://www.googleapis.com/books/v1/volumes?q=inauthor:{query}"}
-
-print("Searching for: ", base_url['book'])
-
-r = requests.get(base_url['book'])
-
-output = r.json()
-tidy_output = []
-for index, item in enumerate(output['items']):
-    # query_results = index, item['id'], item['volumeInfo']['title'],
-    # item['volumeInfo']['authors']
-
-    try:
-        authors = ', '.join(item['volumeInfo']['authors'])
-    except KeyError:
-        authors = "Unknown author"
-
-    tidy_output.append(
-        {
-            index: f"{item['volumeInfo']['title']} - {authors}",
-        }
-    )
+r = {}
+start_index = 0
+query = input("What would you like to search for? ")
 
 
 def niceprint(dct):
-    for person in dct:
-        for k, v in person.items():
+    print("\n")
+    for book in dct:
+        for k, v in book.items():
             print("{: >10} {: >10}".format(k, v))
-            # print("{: >20} {: >20}".format(*row))
+    print("\n")
 
 
-niceprint(tidy_output)
+def search(start_index, query=query):
+    base_url = {
+        'book': f"https://www.googleapis.com/books/v1/volumes?q={query}&startIndex={start_index}",
+        'author': f"https://www.googleapis.com/books/v1/volumes?q=inauthor:{query}"}
 
-moreInfo = int(input(
-    "Enter number of book to read more about, Z: Back, X: Forward "))
+    print("Searching for: ", base_url['book'])
 
-if(type(moreInfo) == int):
-    print(
-        f"Further details on {moreInfo}: {tidy_output[moreInfo]['title']}...")
-    print(output['items'][moreInfo]['volumeInfo']['description'])
+    r.update(requests.get(base_url['book']).json())
+    tidy_output = []
+    for index, item in enumerate(r['items']):
+        try:
+            authors = ', '.join(item['volumeInfo']['authors'])
+        except KeyError:
+            authors = "Unknown author"
+
+        tidy_output.append(
+            {
+                index: f"{item['volumeInfo']['title']} - {authors}",
+            }
+        )
+    niceprint(tidy_output)
+    return tidy_output
+
+
+search(start_index, query)
+
+while(1):
+    moreInfo = input(
+        "Enter number of book to read about, Z: Back, X: Forward, N: Search ")
+
+    try:
+        if(int(moreInfo) >= 0 and int(moreInfo) < 10):
+            moreInfo = int(moreInfo)
+            print(f"Further details on: {moreInfo}...\n")
+            print(
+                f"{r['items'][moreInfo]['volumeInfo']['description'][: 400]}...\n\n")
+
+    except KeyError:
+        print("No description found!")
+
+    except ValueError:
+        if(moreInfo.lower() == "z" and start_index > 0):
+            print("Going back a page...")
+            start_index -= 10
+            search(start_index)
+        elif(moreInfo.lower() == "x"):
+            print("Going forward a page...")
+            start_index += 10
+            search(start_index)
+        elif(moreInfo.lower() == "n"):
+            query = input("What would you like to search for? ")
+            search(0, query)
